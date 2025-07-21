1926fbc53ac3d513680431b23099f3ae4692d9b7
amaan-malik1 <amikm2077@gmail.com>
2025-07-21 16:28:17 +0530

    debug many thing

--- backend/public/assets/latency-chart-Bj5OSYzg.es-DelIfEdd.js
+++ backend/public/assets/latency-chart-Bj5OSYzg.es-DelIfEdd.js
@@ -1,4 +1,4 @@
-import{r as W,R as ti,j as Ue}from"./index-B70gdyJG.js";/*!
+import{r as W,R as ti,j as Ue}from"./index-CBbO6wsU.js";/*!
  * @kurkle/color v0.3.4
  * https://github.com/kurkle/color#readme
  * (c) 2024 Jukka Kurkela
--- backend/src/index.js
+++ backend/src/index.js
@@ -29,10 +29,10 @@ app.use(cors({
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../streamify/dist")));
+  app.use(express.static(path.join(__dirname, "public")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../streamify", "dist", "index.html"));
+  app.get(/^\/((?!api).)*$/, (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
   });
 }
 
